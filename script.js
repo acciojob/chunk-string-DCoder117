909abd0fa9caeef4711097e06f23dd70b4db2207
DCoder117 <dhirajopjadhav17@gmail.com>
2025-12-11 18:29:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function stringChop(str, size) {
   // your code here
+	let arr = [];
+	let temp = size;
+	let j = 0;
+	while(true){
+		for(let i=j; i < size; i--){
+		let chunk += str[i] 
+		}
+		j += size;
+		size += temp;
+		arr.push(chunk);
+		if(size>str.length){
+			break;
+		}
+	}
+	return arr;
 }
 
 // Do not change the code below
